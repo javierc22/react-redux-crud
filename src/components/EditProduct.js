a011ffb967a8fcf6cc31d1e4e34db75b687bab64
javierc22 <javier.ec.22@gmail.com>
2020-08-25 21:23:10 -0400

    Crear edición de productos y Listado

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const EditProduct = () => {
   return (
-    <h2>Desde Editar Producto</h2>
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Editar Producto
+            </h2>
+
+            <form>
+              <div className="form-group">
+                <label>Nombre producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="name" 
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Precio Producto</label>
+                <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Precio Producto"
+                    name="price"
+                />
+              </div>
+
+              <button 
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >Guardar Cambios</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
  
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const Products = () => {
   return (
-    <h2>Desde Productos</h2>
+    <Fragment>
+      <h2 className="text-center my-5">Listado de Productos</h2>
+
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </Fragment>
   );
 }
  
