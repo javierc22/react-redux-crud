0d770304136a7cf43f79d74d5888bad0c002014c
javierc22 <javier.ec.22@gmail.com>
2020-09-08 21:55:22 -0300

    Agregar un 2do reducer (alertReducer)

---
+++ src/reducers/alertReducer.js
@@ -0,0 +1,16 @@
+import {
+  SHOW_ALERT,
+  HIDE_ALERT
+} from "../types"
+
+const initialState = {
+  alert: null
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+  
+    default:
+      return state;
+  }
+}
